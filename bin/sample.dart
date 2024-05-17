23f183609d77b756b1ca8927310ecc248063e3c0
thadathilsinan <thadathilsinan@gmail.com>
2024-05-17 14:51:27 +0530

    Added new variables

--- bin/sample.dart
+++ bin/sample.dart
@@ -1,5 +1,9 @@
 import 'package:sample/sample.dart' as sample;
 
 void main(List<String> arguments) {
+  int a = 10;
+  int b = 50;
+
+  print(a +b);
   print('Hello world: ${sample.calculate()}!');
 }
